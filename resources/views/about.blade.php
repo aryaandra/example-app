3fce173ac75bc8f0574d4ab473848c4c38f74249
aryaandra <aryaandra44@gmail.com>
2024-09-06 14:53:26 +0700

    cobaaja

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <container>
+            <ul>
+                <li><a href="/">Halaman Home</a></li>
+                <li><a href="/about">Halaman About</a></li>
+            </ul>
+        </container>
+    </div>
+    <h1>Halaman About</h1>
+</body>
+
+</html>
